d04130a5073e25212a90248ea74e61c585e299a8
Milton Lash <iblash@live.com>
2023-04-06 21:17:24 +0000

    Photo details and delete photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,4 +7,35 @@ class PhotosController < ApplicationController
 
     render({ :template => "photo_templates/index.html.erb" })
   end
+
+  def show
+    # Parameters: {"path_id" =>"777"}
+
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => url_id })
+
+    @the_photo = matching_photos.at(0)
+
+
+    render({ :template => "photo_templates/show.html.erb"})
+
+
+  end 
+
+  def baii
+    # Parameters: {"toast_id"=>"785"}
+
+    the_id = params.fetch("toast_id")
+
+    matching_photos = Photo.where({ :id => the_id })
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    #render({ :template => "photo_templates/baii.html.erb"})
+    
+    redirect_to("/photos")
+  end
 end
