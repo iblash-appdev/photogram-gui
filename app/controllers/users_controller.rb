62813735bdec3fea934513fab54cd2ca02814eb0
Milton Lash <iblash@live.com>
2023-04-13 16:41:08 +0000

    Users and homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,65 @@ class UsersController < ApplicationController
 
      render({ :template => "user_templates/show.html.erb"})
 
+     def baii
+      # Parameters: {"toast_id"=>"785"}
+  
+      the_id = params.fetch("roast_id")
+  
+      matching_users = User.where({ :id => the_id })
+  
+      the_user = matching_users.at(0)
+  
+      the_user.destroy
+  
+      #render({ :template => "photo_templates/baii.html.erb"})
+      
+      redirect_to("/users")
+    end
+  
+    def create
+  
+      input_user = params.fetch("query_user")
+      #input_caption = params.fetch("query_caption")
+      input_user_id = params.fetch("query_user_id")
+  
+      a_new_user = User.new
+  
+      a_new_user.username = input_username
+        
+      a_new_user.save
+  
+      #render({ :template => "photo_templates/create.html.erb" })
+      
+      redirect_to("/users/" + a_new_user.id.to_s)
+    end
+  
+    def update
+  
+      the_id = params.fetch("modify_id")
+  
+      matching_users =User.where({ :id => the_id})
+  
+      the_user = matching_users.at(0)
+  
+      input_user = params.fetch("query_user")
+      #input_caption = params.fetch("query_caption")
+      #input_owner_id = params.fetch("query_owner_id")
+  
+      the_user.image = input_user
+      #the_photo.caption = input_caption
+  
+      the_user.save
+  
+      #render({ :template => "photo_templates/create.html.erb"})
+      
+      #redirect_to("/photos/" + the_photo.id.to_s)
+  
+      next_url = "/users/" + the_userclass.id.to_s
+  
+      redirect_to(next_url)
+    end
+
   end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "application", :action => "homepage" })
+
   get("/users", { :controller => "users", :action => "index" })
 
   get("/users/:path_username", { :controller => "users", :action => "show" })
+
+  get("/delete_user/:roast_id", { :controller => "users", :action => "baii" })
+
+  get("/insert_user", { :controller => "users", :action => "create" })
+
+  get("/update_user/:modify_id", { :controller => "users", :action => "update"})
+
 
   get("/photos", { :controller => "photos", :action => "index" })
 
